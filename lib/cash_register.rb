c80c247096878772467eb2a89b51292510fd48de
Peter Ayeni <ayenicology@gmail.com>
2019-10-10 06:10:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,47 @@
+class CashRegister
+  
+  attr_accessor :total, :discount, :items_array, :last_item
+  
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+    @items_array = []
+    @last_item = 0.0
+  end
 
+  def add_item(title, price, quantity = 1)
+    self.last_item = price
+    self.total += (price * quantity)
+    quantity.times do 
+      self.items_array.push(title)
+    end
+  end
+  
+  def items
+    self.items_array 
+  end
+  
+  def calculate_discount(discount)
+    return self.total = self.total - (self.total * discount/100)
+  end
+  
+  def apply_discount
+    if self.discount == 0 
+      return "There is no discount to apply."
+    else
+      discounted_total = calculate_discount(self.discount)
+      return "After the discount, the total comes to $#{discounted_total}."
+      
+    end
+  end
+  
+  def void_last_transaction
+    self.total = self.total - self.last_item
+    self.items_array.pop()
+    if self.items_array.empty?
+      self.total = 0.0
+    end
+  end
+
+    
+end
